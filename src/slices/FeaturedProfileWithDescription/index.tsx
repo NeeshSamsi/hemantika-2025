e7653fe84b49f1f39e9070be5fc120b090e59e4d
Neesh <avaneeshsamsi@gmail.com>
2025-10-01 13:50:03 +0200

    feat: Responsive featured profile section

--- src/slices/FeaturedProfileWithDescription/index.tsx
+++ src/slices/FeaturedProfileWithDescription/index.tsx
@@ -1,6 +1,11 @@
 import { FC } from "react"
-import { Content } from "@prismicio/client"
-import { SliceComponentProps } from "@prismicio/react"
+import { asText, Content } from "@prismicio/client"
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react"
+import { PrismicNextImage } from "@prismicio/next"
+import Image from "next/image"
+
+import profileBg from "@/../public/bg-assets/feature-profile.png"
+import bgAsset from "@/../public/bg-assets/feature.png"
 
 /**
  * Props for `FeaturedProfileWithDescription`.
@@ -13,42 +18,58 @@ export type FeaturedProfileWithDescriptionProps =
  */
 const FeaturedProfileWithDescription: FC<
   FeaturedProfileWithDescriptionProps
-> = ({ slice }) => {
+> = ({
+  slice: {
+    slice_type,
+    variation,
+    primary: { image, title, description },
+  },
+}) => {
   return (
     <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
+      data-slice-type={slice_type}
+      data-slice-variation={variation}
+      className="border-light bg-light/8 relative col-span-full mt-8 grid grid-cols-1 rounded border backdrop-blur-xl sm:mt-16 md:mt-32 xl:grid-cols-2"
     >
-      Placeholder component for featured_profile_with_description (variation:{" "}
-      {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
+      <div className="relative hidden xl:block">
+        <PrismicNextImage
+          field={image}
+          className="bottom-0 left-0 w-116 max-w-xl origin-bottom px-8 xl:absolute xl:w-full"
+        />
+        <Image
+          src={profileBg}
+          alt=""
+          className="absolute bottom-0 left-0 -z-10 aspect-square size-220"
+        />
+      </div>
+
+      <div className="space-y-6 p-4 md:px-8 md:py-6">
+        <h2 className="font-heading text-3xl font-bold text-balance lg:text-4xl xl:text-5xl">
+          {asText(title)}
+        </h2>
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-0 xl:block">
+          <div className="prose-base text-light prose-p:leading-normal max-w-none">
+            <PrismicRichText field={description} />
+          </div>
+          <div className="relative block xl:hidden">
+            <PrismicNextImage
+              field={image}
+              className="-bottom-6 left-0 block w-full translate-y-4 sm:w-2/3 md:translate-y-6 lg:absolute lg:w-full lg:translate-y-0 lg:pl-6"
+            />
+            <Image
+              src={profileBg}
+              alt=""
+              className="absolute -right-12 bottom-6 -z-10 hidden aspect-square size-160 lg:block"
+            />
+          </div>
+        </div>
+      </div>
+
+      <Image
+        src={bgAsset}
+        alt=""
+        className="absolute -right-50 -bottom-48 -z-10 w-120 sm:-right-80 sm:-bottom-64 xl:-right-96 xl:-bottom-72 xl:w-180"
+      />
     </section>
   )
 }
